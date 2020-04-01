1e5d9704ff7e6ed5ce488d09459b9f4e9700dbae
stanislawantoniak <stanislaw.h.antoniak@gmail.com>
2020-04-01 10:50:06 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const gateway = new ApolloGateway();
-const server = new ApolloServer({
-  gateway,
-  engine: {
-    apiKey: process.env.AGM_API_KEY,
-    schemaTag: process.env.AGM_SCHEMA_TAG
-  }
-});
