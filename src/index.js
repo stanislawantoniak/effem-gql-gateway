87b629f9fe7fd700a586706b8cbf50d2ff213bb4
Stanislaw Antoniak <stanislaw.h.antoniak@gmail.com>
2020-04-02 08:13:42 +0200

    first working version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log('api key: ', process.env.AGM_API_KEY);
+
 const gateway = new ApolloGateway();
+
 const server = new ApolloServer({
-  gateway,
-  engine: {
-    apiKey: process.env.AGM_API_KEY,
-    schemaTag: process.env.AGM_SCHEMA_TAG
-  }
+	gateway,
+	engine: {
+		apiKey: process.env.AGM_API_KEY,
+		schemaTag: process.env.AGM_SCHEMA_TAG
+	},
+	subscriptions: false,
+});
+
+server.listen({ port: process.env.PORT || 4010 }).then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
 });
